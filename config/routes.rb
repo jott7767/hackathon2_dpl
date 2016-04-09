f331660da1635ad42f697f22a0ed97f8b712c861
GavMan1995 <gavyncaldwell@gmail.com>
2016-04-09 17:27:09 -0600

    routes and api key

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  
   root 'home#index'
 
-  resources :clients do
-    resources :orders
-  end
-
+  resources :clients
   resources :inventories
   resources :shipments
 
